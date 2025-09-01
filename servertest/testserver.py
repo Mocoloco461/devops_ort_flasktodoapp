005782a26ff442f61ffa7129111ebfbee13d4880
Mocoloco461 <nuhnuh0101@gmail.com>
2025-09-01 14:36:25 +0300

    new test workflow

---
+++ servertest/testserver.py
@@ -0,0 +1,22 @@
+import requests
+import time
+
+url = "http://flask_app:5000/add"
+data = {"title": "לסיים את פרויקט Flask"}
+
+start_time = time.time()
+success = False
+
+while time.time() - start_time < 60:  # עד דקה
+    try:
+        response = requests.post(url, data=data)
+        if response.status_code == 200:
+            print("Success!")
+            success = True
+            break
+    except requests.exceptions.ConnectionError:
+        pass
+    time.sleep(2)
+
+if not success:
+    print("Error: not working")
